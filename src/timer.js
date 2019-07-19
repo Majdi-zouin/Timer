cee8e8ab24c079f1907fce4898a3bb7349672506
Majdi-zouin <majdizouin@gmail.com>
2019-07-19 15:09:22 +0200

    first commit

---
+++ src/timer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './timer.css';
+class Timer extends Component {
+    constructor(props) {
+        super (props);
+        this.state = {  }
+    }
+    render() {
+        return (<div>
+            <div className='Timer'>
+            
+                <h1>00:30:00</h1>
+                <h3>Hour Minute Second</h3>    
+            </div>
+
+        </div>)
+    }
+}
+
+
+
+
+export default Timer;
